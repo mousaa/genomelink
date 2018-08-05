0e603113c7d96369fdfe30642dd8b7088d117399
Ahmed <mousaahmed96@gmail.com>
2018-08-04 20:05:59 -0400

    database setup

--- Podfile
+++ Podfile
@@ -3,6 +3,7 @@
 
 target 'genomelinks' do
   pod 'Firebase/Core'
+  pod 'Firebase/Database'	
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
--- genomelinks/ViewController.swift
+++ genomelinks/ViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .red
+        super.viewDidLoad()        
     }
 
     override func didReceiveMemoryWarning() {
