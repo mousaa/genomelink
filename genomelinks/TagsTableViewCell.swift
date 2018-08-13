fe6d770b12ec554b1e93626b0b943c71c57fc835
Jason Long <jasonlong98@gmail.com>
2018-08-13 17:55:52 -0400

    did all basic set up

---
+++ genomelinks/TagsTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  TagsTableViewCell.swift
+//  genomelinks
+//
+//  Created by Jason Long on 2018-08-13.
+//  Copyright © 2018 Ahmed Mousa. All rights reserved.
+//
+
+import UIKit
+
+class TagsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    func setCell() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.showsHorizontalScrollIndicator = false
+    }
+}
+
+extension TagsTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TagsCell", for: indexPath) as! TagsViewCell
+        cell.clipsToBounds = true
+        cell.layer.cornerRadius = 10.0
+        
+        cell.label.text = "goose"
+        cell.image.image = UIImage(named:"goose")!
+        
+        return cell
+    }
+}
