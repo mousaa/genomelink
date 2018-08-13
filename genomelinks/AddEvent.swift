758c7eeb8658faad3e09b07ab55256329142e7f5
Ahmed Mousa <mousaahmed96@gmail.com>
2018-08-13 18:47:29 -0400

    fix main story

---
+++ genomelinks/AddEvent.swift
@@ -0,0 +1,20 @@
+//
+//  AddEvent.swift
+//  genomelinks
+//
+//  Created by ahmed mousa on 2018-08-13.
+//  Copyright © 2018 Ahmed Mousa. All rights reserved.
+//
+
+import UIKit
+
+class AddEvent: UIViewController {
+    override func viewDidLoad() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem()
+        navigationItem.rightBarButtonItem?.title = "hehe"
+        navigationItem.title = "Add Event"
+        navigationItem.backBarButtonItem = UIBarButtonItem()
+        navigationItem.backBarButtonItem?.title = "Back"
+    }
+}
+
