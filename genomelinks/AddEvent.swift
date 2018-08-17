90c29cfb5ebf116b93b80b4042302789569b62d4
Ahmed <mousaahmed96@gmail.com>
2018-08-17 19:35:41 -0400

    inital commit

--- genomelinks/AddEvent.swift
+++ genomelinks/AddEvent.swift
@@ -9,14 +9,12 @@
 import UIKit
 
 class AddEvent: UIViewController {
-    
-    @IBAction func callAPI(_ sender: Any) {
-    }
-    
     override func viewDidLoad() {
+        navigationItem.title = "Add Event"
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem()
         navigationItem.rightBarButtonItem?.title = "Create"
-        navigationItem.title = "Add Event"
+        
         navigationItem.backBarButtonItem = UIBarButtonItem()
         navigationItem.backBarButtonItem?.title = "Cancel"
     }
